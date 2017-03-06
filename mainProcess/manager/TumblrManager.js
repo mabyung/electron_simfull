e5f4ba6b629f7ef7e397f8ed5431723220b91b72
mabyung <mabyung@naver.com>
2017-03-07 01:10:16 +0900

    init commit

--- mainProcess/manager/TumblrManager.js
+++ mainProcess/manager/TumblrManager.js
@@ -12,7 +12,7 @@ class TumblrManager
             const post = new Browser({show : true, width : 1600, height : 720 });
             post.goto("https://www.tumblr.com/new/video", { contextIsolation : true });
             yield sleep(4000);
-            var src = "https://www.youtube.com/watch?v=GeGypUVvZ5c";
+            const src = "https://www.youtube.com/watch?v=GeGypUVvZ5c";
             const first = yield post.execute( function( _src ) {
                     document.querySelector(".media-url-button").click();
                     ( ()=> {
@@ -31,7 +31,7 @@ class TumblrManager
                     setTimeout( ()=> document.execCommand('Paste') , 1000 );
                     return document.querySelector(".media-url-button").innerHTML;
             }, [src] );
-            console.dir( first );
+            console.dir( "first : ", ~first );
             // post.close();
         }).then((r)=> console.log( "TumblrManager completed" ), err => console.error('OH NOES!', err) );
     }
