bac9562a467476957206c78e91336a49a854c57e
MrChoiSSi <mrchoissi@nate.com>
2017-03-17 17:28:07 +0900

    파일 수정

--- mainProcess/getObjData.js
+++ mainProcess/getObjData.js
@@ -6,13 +6,13 @@ let downloadManager = require("./manager/SFDownloadManager");
 let uploadManager = require("./manager/SFUploadManager");
 let ytHelper = require("./manager/SFImageHelper").ytHelper;
 let tumblrManager = require("./manager/TumblrManager");
-let daysData = JSON.parse(fs.readFileSync('postDays.json', 'utf-8'));
 
 let handleDownloadComplete = ( _title, _category, _results ) =>{
     uploadManager.uploadFTP( _title, _category , _results );
 };
 // 받기
 ipc.on('community-data', (e, arg) => {
+    console.log(arg)
     downloadManager.insertFile( arg.img, arg.title, arg.category, handleDownloadComplete );
 });
 
@@ -21,9 +21,11 @@ ipc.on("yt-data", (e, arg) =>{
 });
 
 ipc.on("upload-click", (e, arg) =>{
-    for(var i = 0; i < arg.length; i++){
-        console.log(daysData[arg[i]]);
-        handleDownloadComplete( daysData[arg[i]].title, daysData[arg[i]].category, [daysData[arg[i]].fileName] );
-    }
+    let daysData = JSON.parse(fs.readFileSync('postDays.json', 'utf-8'));
+    ytHelper.uploadGif( arg, daysData[arg].title, daysData[arg].category, daysData[arg].fileName, handleDownloadComplete );
+    //for(var i = 0; i < arg.length; i++){
+    //    console.log(daysData[arg[i]]);
+    //    ytHelper.uploadGif( daysData[arg[i]].title, daysData[arg[i]].category, [daysData[arg[i]].fileName], handleDownloadComplete );
+    //}
 });
 
--- mainProcess/manager/SFImageHelper.js
+++ mainProcess/manager/SFImageHelper.js
@@ -1,5 +1,6 @@
 const YT = require("youtube.com");
 let fs = require('fs');
+let methodQueue = require("./../methodQueue");
 let daysData = JSON.parse(fs.readFileSync('postDays.json', 'utf-8'));
 
 let YtHelper = class YtHelper{
@@ -15,16 +16,28 @@ let YtHelper = class YtHelper{
         this.youtube = YT(opt.url);
         this.youtube
         .gif( opt.startDuration, opt.endDuration, fileName, "640x360", 30 )
-        // .then( ()=> completeFunc( opt.title, opt.category, [ _fName ] )  )
+            // .then( ()=> completeFunc( opt.title, opt.category, [ _fName ] )  )
+        .then( ()=> {
+            let postData = { category:opt.category, title:opt.title, fileName:_fName };
+            daysData.push(postData);
+            let string = JSON.stringify(daysData);
+            fs.writeFile('postDays.json', string, 'utf8', function(error){
+                if(error) return console.log(error);
+                console.log('Write Complete');
+            });
+        } )
         .catch( (err)=> console.log("err : ", err) )
+    }
 
-        let postData = { category:opt.category, title:opt.title, fileName:_fName };
-        daysData.push(postData);
-        let string = JSON.stringify(daysData);
-        fs.writeFile('postDays.json', string, 'utf8', function(error){
-            if(error) return console.log(error);
-            console.log('Write Complete');
-        });
+    uploadGif( title, cate, fileName, onComplete )
+    {
+        this.queue = new methodQueue();
+        this.results = [];
+        //urls.forEach( (elem, i ) =>{
+        //    this.queue.addItem( onComplete , false,  [ title[i], cate[i], [fileName[i]] ]);
+        //});
+       this.queue.addItem( onComplete , false,  [ title, cate, [fileName] ]);
+        this.queue.next();
     }
 }
 
